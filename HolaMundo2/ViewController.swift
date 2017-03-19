fe2d20d8626da6e4cbfc52ce6a85e6800e248992
“spanesso” <spanesso@gmail.com>
2017-03-19 13:29:18 -0500

    Initial Commit

---
+++ HolaMundo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo2
+//
+//  Created by Sebastian Alfredo Panesso on 19/03/17.
+//  Copyright © 2017 Nextuniversity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
