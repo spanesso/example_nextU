08ddd37e188f84f46dea9f71717109c496282303
“spanesso” <spanesso@gmail.com>
2017-03-24 01:18:52 -0500

    comentary

--- HolaMundo2/ViewController.swift
+++ HolaMundo2/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameMessage: UILabel!
+    @IBOutlet weak var nameFiled: UITextField!
+  
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +22,17 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    
+    @IBAction func change(sender: UIButton) {
+        nameMessage.text = "Hola \(nameFiled.text!)"
+        view.endEditing(true)
+        
+    }
+    
+    //Comentary
+    
+ 
 }
 
